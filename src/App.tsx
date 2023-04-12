6bcd6a5b3c820254503df2a0dd316e106673cbdb
Bruno Gomes <gomesbruno030@outlook.com>
2023-04-12 14:25:22 -0300

    feat - ajusting css

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
     letras.push(String.fromCharCode(i));
   }
 
+  const dica = "um animal"
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,21 +27,34 @@ function App() {
         <div className="pernaDireito"></div>
         <div className="pernaEsquerdo"></div>
 
+        <div className="conteinerDica">
+          <span className="textDica">
+            dica: <span>{dica}</span>
+          </span>
+        </div>
+
         <div className="box1">
           <div className="box2">
 
-            <div className="box3">
-              dica:fdfds
-            </div>
+        <div className="box3"></div>
 
+        <div className="conteinerResposta">
+          <span className="textResposta">
+            Gato
+          </span>
+        </div>
+
+
+          <div className="conteinerTeclado2">
             <div className="conteinerTeclado">
-              <div className="teclado">
-                {letras.map((iten, index) => (
-                  <div className="tecla" key={index}>{iten}</div>
-                ))}
+                <div className="teclado">
+                  {letras.map((iten, index) => (
+                    <div className="tecla" key={index}>{iten}</div>
+                  ))}
+                </div>
               </div>
-            </div>
-
+          </div>
+          
           </div>
         </div>
         {/* <div className='box2'></div> */}
